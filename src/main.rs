d94a21dff4b49d535850e053a5da2c65b20b8397
Mara Schulke <mara@schulke.xyz>
2019-02-06 23:05:01 +0100

    added counting-sort

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ mod sort;
 
 fn main() {
 	/* gen an array containing random numbers */
-	let mut random_array: [u8; 20] = [
-		214, 12, 145, 135, 32, 0, 255, 233, 231, 123, 224, 123, 55, 32, 43, 32, 52, 43, 21, 85,
+	let mut random_array: [u32; 20] = [
+		214, 12, 145, 135, 32, 1, 255, 233, 231, 123, 224, 123, 55, 32, 43, 32, 52, 43, 21, 85,
 	]; //rand::thread_rng().gen();
 
 	println!("unsorted {:?}", random_array);
--- src/sort/counting_sort.rs
+++ src/sort/counting_sort.rs
@@ -1,16 +1,39 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
-pub fn counting_sort(arr: &mut [u8], maxval: usize) {
-    let mut occurances = vec![0; maxval + 1];
-    let mut index = vec![0; maxval + 1];
+pub fn counting_sort(arr: &mut [u32], maxval: usize) {
+    let mut occurences: Vec<u32> = vec![0; maxval + 1];
+    let mut index: Vec<u32> = vec![0; maxval + 1];
 
-    for i in arr {
-        occurances[*i as usize] += 1;
-        println!("{}", occurances[*i as usize]);
-        // println!("{}", count[*i as usize - 1])
+    for &i in arr.iter() {
+        occurences[i as usize] += 1;
     }
 
-    for occured in occurances {
-        index[*occured as usize] = 0x04;
+    let mut calculated_index: u32 = 0;
+
+    for j in 0..maxval + 1 {
+        calculated_index += occurences[j];
+
+        index[j] += calculated_index;
     }
+
+    let mut sorted: Vec<u32> = vec![0; arr.len()];
+
+    for k in arr.iter() {
+        sorted[(index[*k as usize] - 1) as usize] = *k;
+
+        index[*k as usize] -= 1;
+        // arr[]
+        // index[k] -= 1;
+    }
+
+    for x in 0..arr.len() {
+        arr[x] = sorted[x];
+    }
+
+    // arr = sorted;
+
+    println!("{:?}", sorted);
+
+    println!("\noccurances \n\n {:?} \n", occurences);
+    println!("indices \n\n {:?} \n", index);
 }
