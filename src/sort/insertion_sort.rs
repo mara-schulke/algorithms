557e04afd639b8909b2366ba6e106f5982e28bd9
Mara Schulke <mara@schulke.xyz>
2019-02-11 18:22:37 +0100

    feat: implemented insertion sort

--- src/sort/insertion_sort.rs
+++ src/sort/insertion_sort.rs
@@ -2,15 +2,14 @@
 #[allow(unused_variables)]
 pub fn insertion_sort(arr: &mut [u8]) {
 	for i in 1..arr.len() {
-		println!("{}", arr[i as usize]);
+		let mut j: u32 = i as u32;
 
-		let mut j: u8 = i as u8;
-
-		while j > 1 && arr[(j - 1) as usize] > arr[i] {
+		while j > 0 && arr[j as usize] < arr[(j - 1) as usize] {
+			let temp = arr[j as usize];
 			arr[j as usize] = arr[(j - 1) as usize];
+			arr[(j - 1) as usize] = temp;
 
 			j -= 1;
 		}
-		arr[j as usize] = arr[i];
 	}
 }
