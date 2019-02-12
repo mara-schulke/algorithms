6e667dbaf2e7d8c7d194709a11fba4d5970dd393
Mara Schulke <mara@schulke.xyz>
2019-02-12 08:30:42 +0100

    feat: implemented selection sort

--- src/sort/selection_sort.rs
+++ src/sort/selection_sort.rs
@@ -1,5 +1,19 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
-pub fn selection_sort() {
-    unimplemented!();
+pub fn selection_sort(arr: &mut [u8]) {
+    for i in 0..arr.len() - 1 {
+        let mut index_of_minimum: usize = 0;
+
+        for j in i..arr.len() {
+            if arr[j] < arr[index_of_minimum] {
+                index_of_minimum = j;
+            }
+        }
+
+        if index_of_minimum != i {
+            let temp = arr[index_of_minimum];
+            arr[index_of_minimum] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
