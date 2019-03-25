8ae46311e41895977ada20ccf8409019fb8f56dc
Mara Schulke <mara@schulke.xyz>
2019-03-25 20:57:57 +0100

    feat(quick_sort): implemeted a fully working version of wuick-sort

--- src/sorting/quick_sort.rs
+++ src/sorting/quick_sort.rs
@@ -1,72 +1,31 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn quick_sort(arr: &mut [u8]) {
-    let len = arr.len() - 1;
-
-    qs(arr, 0, len as isize);
+    qs(arr, 0, (arr.len() - 1) as isize);
 }
 
-fn qs(arr: &mut [u8], low: isize, high: isize) {
-    if low < high {
-        let pivot: isize = partition(arr, low, high);
+fn qs(arr: &mut [u8], lower: isize, upper: isize) {
+    if upper > lower {
+        let pivot = partition(arr, lower, upper);
 
-        qs(arr, low, pivot - 1);
-        qs(arr, pivot + 1, high);
+        qs(arr, lower, pivot - 1);
+        qs(arr, pivot + 1, upper);
     }
 }
 
-fn partition(arr: &mut [u8], low: isize, high: isize) -> isize {
-    let mut i: isize = low as isize - 1;
+fn partition(arr: &mut [u8], lower: isize, upper: isize) -> isize {
+    let pivot = arr[upper as usize];
 
-    for j in 0..high - 1 {
-        if arr[j as usize] <= arr[high as usize] {
+    let mut i = lower - 1;
+    for j in lower..upper {
+        if arr[j as usize] <= pivot {
             i += 1;
+
             arr.swap(i as usize, j as usize);
         }
     }
-    println!("High{} Low{}", high, low);
 
-    arr.swap((i + 1) as usize, high as usize);
+    arr.swap((i + 1) as usize, upper as usize);
 
     i + 1
 }
-
-/*
-int Partition (int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low -1);
-
-    for (int j = low; j <= (high - 1); j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            Swap (&arr[i], &arr[j]);
-        }
-    }
-
-    Swap(&arr[i + 1], &arr[high]);
-
-    return (i + 1);
-}
-
-void QuickSort(int arr[], int low, int high)
-{
-    int pivot;
-
-    if (low < high) {
-        pivot = Partition(arr, low, high);
-        QuickSort(arr, low, pivot - 1);
-        QuickSort(arr, pivot + 1, high);
-    }
-}
-
-int main(int argc, char* argv[])
-{
-   /* Call Main sorting algo */
-QuickSort(arr, 0, count - 1);
-
-printf("Quick sorted array\n");
-PrintArray(arr, 0, count, NOPIVOT);
-return 0;
-}
-
-*/
