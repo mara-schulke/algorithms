4eb7ec4809015e9b038b03308ab65b1b76498c2f
Mara Schulke <mara@schulke.xyz>
2021-06-24 23:03:40 +0200

    Implement insertion sort in two different ways

--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -1,11 +1,32 @@
-pub fn insertion_sort(arr: &mut [u8]) {
-    for i in 1..arr.len() {
-        let mut j: usize = i;
+pub fn insertion_sort<T: Ord>(arr: &mut [T]) {
+    self::optimized::insertion_sort(arr);
+}
 
-        while j > 0 && arr[j] < arr[j - 1] {
-            arr.swap(j, j - 1);
+mod traditional {
+    #[allow(dead_code)]
+    pub fn insertion_sort<T: Ord>(arr: &mut [T]) {
+        for unsorted in 1..arr.len() {
+            let mut i = unsorted;
 
-            j -= 1;
+            while i > 0 && arr[i - 1] > arr[i] {
+                arr.swap(i - 1, i);
+                i -= 1;
+            }
         }
     }
+
+    crate::test_sort!(insertion_sort);
+}
+
+mod optimized {
+    pub fn insertion_sort<T: Ord>(arr: &mut [T]) {
+        (1..arr.len()).for_each(|unsorted| {
+            let target_index = arr[..unsorted]
+                .binary_search(&arr[unsorted])
+                .into_ok_or_err();
+            arr[target_index..=unsorted].rotate_right(1);
+        });
+    }
+
+    crate::test_sort!(insertion_sort);
 }
