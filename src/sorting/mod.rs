f833b79c6b82e06100c2480ab245e73a8c89ace0
Mara Schulke <mara@schulke.xyz>
2021-06-24 23:01:48 +0200

    Add auto generated tests for different sorting algorithms

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -4,6 +4,7 @@ mod counting_sort;
 mod gnome_sort;
 mod heap_sort;
 mod insertion_sort;
+mod merge_sort;
 mod quick_sort;
 mod radix_sort;
 mod selection_sort;
@@ -21,3 +22,80 @@ pub use self::radix_sort::radix_sort;
 pub use self::selection_sort::selection_sort;
 pub use self::shell_sort::shell_sort;
 pub use self::slow_sort::slow_sort;
+
+#[macro_export]
+macro_rules! gen_sort_tests {
+    ($alg:ident, $($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (mut input, expected) = $value;
+                $alg(&mut input);
+                assert_eq!(input, expected);
+            }
+        )*
+    }
+}
+
+#[macro_export]
+macro_rules! test_sort (
+    ($alg:ident) => {
+        #[cfg(test)]
+        mod test {
+            use super::$alg;
+            use crate::gen_sort_tests;
+
+            gen_sort_tests! {
+                $alg,
+                empty: (
+                    vec![] as Vec<u8>,
+                    vec![] as Vec<u8>
+                ),
+                single: (
+                    vec![1], vec![1]
+                ),
+                sorted: (
+                    vec![1, 2, 3, 4],
+                    vec![1, 2, 3, 4]
+                ),
+                slightly_unsorted: (
+                    vec![1, 3, 2, 4],
+                    vec![1, 2, 3, 4]
+                ),
+                even_unsorted: (
+                    vec![6, 21, 1, 2],
+                    vec![1, 2, 6, 21]
+                ),
+                even_sorted: (
+                    vec![1, 2, 6, 21],
+                    vec![1, 2, 6, 21]
+                ),
+                odd_unsorted: (
+                    vec![1, 2, 0],
+                    vec![0, 1, 2]
+                ),
+                odd_sorted: (
+                    vec![0, 1, 2],
+                    vec![0, 1, 2]
+                ),
+                large_numbers_sorted: (
+                    vec![124122, 401291, 4192421, 22131293, 124214124],
+                    vec![124122, 401291, 4192421, 22131293, 124214124]
+                ),
+                large_numbers_unsorted: (
+                    vec![124122, 24192421, 2131293, 124214124, 401291],
+                    vec![124122, 401291, 2131293, 24192421, 124214124]
+                ),
+                strings: (
+                    vec!["a", "b", "d", "c"],
+                    vec!["a", "b", "c", "d"],
+                ),
+                complex_strings: (
+                    vec!["a2", "a1", ".", "1c"],
+                    vec![".", "1c", "a1", "a2"],
+                ),
+            }
+        }
+    }
+);
+
